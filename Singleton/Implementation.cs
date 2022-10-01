363715b2ee8c65f373493d615fcb03220f661796
Lucas F Marçoni Ferreira <lucasfmarconi@gmail.com>
2022-10-01 18:35:36 +0200

    Add initial singleton impl

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,4 +1,27 @@
-﻿namespace Singleton
+﻿namespace Singleton;
+
+public class Logger
 {
-  
+    private static Logger? _instance;
+
+    public static Logger Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+    }
+
+    protected Logger()
+    {
+    }
+
+    public void Log(string logMessage)
+    {
+        Console.WriteLine($"Message to log: {logMessage}");
+    }
 }
